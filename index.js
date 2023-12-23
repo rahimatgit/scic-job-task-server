f184bdc2f66bb94550954835255f0a9d749bc831
rahimatgit <rahimmonjur9@gmail.com>
2023-12-22 19:38:55 -0800

    crud operation done

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// jobTask
-// WzHMceLORi4A3NgB
-
+// https://scic-job-task-server-mu.vercel.app/
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgmtqy5.mongodb.net/?retryWrites=true&w=majority`;    
 
@@ -26,13 +24,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const toDoCollection = client.db('jobTask').collection('toDo');
+    const ongoingCollection = client.db('jobTask').collection('ongoing');
+    const doneCollection = client.db('jobTask').collection('done');
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
